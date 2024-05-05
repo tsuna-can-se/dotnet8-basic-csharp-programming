b165b4f067217cfaffc1fe41f0465000774d57df
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2024-05-05 12:04:16 +0900

    required修飾子のサンプルを追加 (#5)

---
+++ text/10/ConstructorSample/Book2.cs
@@ -0,0 +1,13 @@
+﻿namespace ConstructorSample;
+
+public class Book2
+{
+    public required string Title { get; set; }
+
+    public required string Author { get; set; }
+
+    public override string ToString()
+    {
+        return $"Title:{this.Title}, Author:{this.Author}";
+    }
+}
