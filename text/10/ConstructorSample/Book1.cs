3393808714b8795588d33e23376f3c2541efa518
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2024-05-05 11:06:54 +0900

    required修飾子のサンプルを追加

---
+++ text/10/ConstructorSample/Book1.cs
@@ -0,0 +1,19 @@
+﻿namespace ConstructorSample;
+
+public class Book1
+{
+    public Book1(string title, string author)
+    {
+        this.Title = title;
+        this.Author = author;
+    }
+
+    public string Title { get; set; }
+
+    public string Author { get; set; }
+
+    public override string ToString()
+    {
+        return $"Title:{this.Title}, Author:{this.Author}";
+    }
+}
--- text/10/ConstructorSample/Program.cs
+++ text/10/ConstructorSample/Program.cs
@@ -13,3 +13,12 @@ Point p1_2 = new Point(1, 2);
 
 Console.WriteLine(p0_1);
 Console.WriteLine(p1_2);
+
+Book1 book1 = new Book1("Visual Studioの本", "たろう");
+Book2 book2 = new Book2() { Title = "C#の本", Author = "はなこ" };
+
+// コンパイルエラーになる例
+// Book2 book2 = new Book2() { Title = "C#の本" };
+
+Console.WriteLine(book1);
+Console.WriteLine(book2);
